f11c8dfa2e9817ad22bc9a456612ccd21449aadd
aswatson <aswatson95@gmail.com>
2024-12-04 13:52:13 -0700

    Contains problem, chosen list repeats entries

--- app/src/main/java/com/sabredg/android/foodtrail/model/Chosen.kt
+++ app/src/main/java/com/sabredg/android/foodtrail/model/Chosen.kt
@@ -1,8 +1,15 @@
 package com.sabredg.android.foodtrail.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Chosen(
-    val chosen: MutableList<Choice>
-) {
+    val chosen: MutableList<Choice> = mutableListOf()
+) : Parcelable {
+    val chosenList: List<Choice>
+        get() = chosen.toList()
+
     fun addChoice(choice: Choice): Chosen {
         val updatedChosen = chosen.toMutableList().apply { add(choice) }
         return copy(chosen = updatedChosen)
--- app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
+++ app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
@@ -3,55 +3,71 @@ package com.sabredg.android.foodtrail.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sabredg.android.foodtrail.R
 import com.sabredg.android.foodtrail.model.Choice
-
-@Preview
-@Composable
-private fun ChoicesPagePreview() {
-    ChoicesPage()
-}
+import com.sabredg.android.foodtrail.model.Chosen
 
 @Composable
 fun ChoicesPage(
     modifier: Modifier = Modifier
 ) {
+    var chosen by rememberSaveable { mutableStateOf(Chosen()) }
 
     Column(
         modifier = modifier
     ) {
         ChoicesList(
-            modifier = Modifier
+            chosen = chosen,
+            onEditChosen = { chosen = it },
+            modifier = modifier
                 .padding(16.dp)
                 .fillMaxWidth()
         )
+
+        ChosenList(
+            chosen = chosen,
+            modifier = modifier
+                .fillMaxWidth()
+        )
+
         SubmitButton()
     }
 }
 
 @Composable
 private fun ChoicesList(
-    modifier: Modifier = Modifier.Companion
+    chosen: Chosen,
+    onEditChosen: (Chosen) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+
+    var choiceAdded by rememberSaveable { mutableStateOf<Choice?>(null) }
+
+    choiceAdded?.let { choice ->
+        onEditChosen(chosen.addChoice(choice))
+    }
+
     LazyColumn(
         modifier = modifier
     ) {
+
         items(Choice.entries.toTypedArray()) { choice ->
             Button(
-                onClick = {}
+                modifier = modifier,
+                onClick = { choiceAdded = choice }
             ) {
                 Text(text = choice.toString())
             }
@@ -60,19 +76,28 @@ private fun ChoicesList(
 }
 
 @Composable
-private fun SubmitButton(
-    modifier: Modifier = Modifier.Companion
+private fun ChosenList(
+    chosen: Chosen,
+    modifier: Modifier = Modifier
 ) {
-    TextButton(
-        modifier = modifier,
+    LazyRow(
+        modifier = modifier
+    ) {
+        items(chosen.chosenList) {choice ->
+            Text(text = choice.toString())
+        }
+    }
+}
+
+@Composable
+private fun SubmitButton(
+    modifier: Modifier = Modifier
+) {
+    Button(
+        modifier = modifier
+            .fillMaxWidth(),
         onClick = { /**TODO**/ }
     ) {
         Text(text = stringResource(R.string.submit_choices_button))
     }
 }
-
-@Composable
-private fun ComposableButton(
-
-    modifier: Modifier = Modifier
-) {}
