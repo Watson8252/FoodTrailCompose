9e9a95f1547a3d19e32f6cf3cb5f7507fc4f5dd7
aswatson <aswatson95@gmail.com>
2024-12-04 12:35:39 -0700

    Fixed a problem with creating the lazy column items.

--- app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
+++ app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
@@ -1,16 +1,23 @@
 package com.sabredg.android.foodtrail.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sabredg.android.foodtrail.R
 import com.sabredg.android.foodtrail.model.Choice
-import com.sabredg.android.foodtrail.model.Chosen
 
 @Preview
 @Composable
@@ -20,28 +27,33 @@ private fun ChoicesPagePreview() {
 
 @Composable
 fun ChoicesPage(
-    modifier: Modifier = Modifier.Companion
+    modifier: Modifier = Modifier
 ) {
+
     Column(
         modifier = modifier
     ) {
+        ChoicesList(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
         SubmitButton()
     }
 }
 
 @Composable
 private fun ChoicesList(
-    chosen: Chosen,
     modifier: Modifier = Modifier.Companion
 ) {
     LazyColumn(
         modifier = modifier
     ) {
-        items(Choice.values().toList()) { choice ->
-            TextButton(
+        items(Choice.entries.toTypedArray()) { choice ->
+            Button(
                 onClick = {}
             ) {
-                Text(choice.toString())
+                Text(text = choice.toString())
             }
         }
     }
@@ -58,3 +70,9 @@ private fun SubmitButton(
         Text(text = stringResource(R.string.submit_choices_button))
     }
 }
+
+@Composable
+private fun ComposableButton(
+
+    modifier: Modifier = Modifier
+) {}
