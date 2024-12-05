30c8c6b6e236a04fc929c64c520e5cf3dd0b38ea
aswatson <aswatson95@gmail.com>
2024-12-05 14:02:03 -0700

    modifiers adjusted

--- app/src/main/java/com/sabredg/android/foodtrail/MainActivity.kt
+++ app/src/main/java/com/sabredg/android/foodtrail/MainActivity.kt
@@ -7,12 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.sabredg.android.foodtrail.model.Chosen
 import com.sabredg.android.foodtrail.ui.ChoicesPage
 import com.sabredg.android.foodtrail.ui.theme.FoodTrailTheme
 
@@ -23,12 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-            var chosen by rememberSaveable { mutableStateOf(Chosen()) }
-
             FoodTrailTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    ChoicesPage(chosen = chosen)
+                    ChoicesPage()
                 }
             }
         }
--- app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
+++ app/src/main/java/com/sabredg/android/foodtrail/ui/ChoicesPage.kt
@@ -1,5 +1,8 @@
 package com.sabredg.android.foodtrail.ui
 
+import android.widget.GridView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,17 +22,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sabredg.android.foodtrail.R
 import com.sabredg.android.foodtrail.model.Choice
 import com.sabredg.android.foodtrail.model.Chosen
 
+@Preview
 @Composable
 fun ChoicesPage(
-    modifier: Modifier = Modifier,
-    chosen: Chosen
+    modifier: Modifier = Modifier
 ) {
+    var chosen by rememberSaveable { mutableStateOf(Chosen()) }
 
     Column(
         modifier = modifier.fillMaxSize()
@@ -37,7 +45,7 @@ fun ChoicesPage(
             chosen = chosen,
             onEditChosen = { chosen = it },
             modifier = modifier
-                .padding(16.dp)
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
                 .fillMaxWidth()
         )
 
@@ -65,10 +73,22 @@ private fun ChoicesList(
             onEditChosen(chosen.addChoice(choice))
         }
     }
-    Text(text = "Choose From:")
+    Text(
+        text = "Choose From:",
+        modifier = modifier.padding(top = 24.dp)
+    )
 
-    LazyColumn(
+    LazyColumn (
         modifier = modifier
+            .border(
+                width = 2.dp,
+                color = Color.DarkGray,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .background(
+                Color.LightGray,
+                shape = RoundedCornerShape(16.dp)
+            )
             .height(400.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -93,7 +113,10 @@ private fun ChosenList(
     var choiceRemoved by rememberSaveable { mutableStateOf<Choice?>(null) }
 
     choiceRemoved?.let { choice ->
-        onEditChosen(chosen.removeChoice(choice))
+        if (chosen.chosenList.contains(choice)){
+            onEditChosen(chosen.removeChoice(choice))
+        }
+        choiceRemoved = null
     }
 
     LazyRow(
@@ -102,8 +125,9 @@ private fun ChosenList(
     ) {
         items(chosen.chosenList) {choice ->
             Button(
-                modifier = Modifier,
-                onClick = {}
+                modifier = Modifier
+                    .padding(4.dp),
+                onClick = { choiceRemoved = choice }
             ){
                 Text(text = choice.toString())
             }
